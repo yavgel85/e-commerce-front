726e77011bc7c2295e912d00f98b04a95a8e4099
Eugene Yavgel <eugene.yavgel@gmail.com>
2021-03-04 23:56:01 +0200

    Optimizing image loading

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
         <div class='card equal-height'>
           <div class='card-content is-flex is-justify-content-center'>
             <figure class='image'>
-              <img src="https://images.unsplash.com/photo-1421882402971-b18cd1741ac6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2688&q=80" alt="Product name">
+              <img decoding="async" loading="lazy" src="https://images.unsplash.com/photo-1421882402971-b18cd1741ac6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2688&q=80" alt="Product name">
             </figure>
           </div>
         </div>
