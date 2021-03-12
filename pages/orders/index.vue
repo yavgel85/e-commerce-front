ec32228be908feb0ec3171c6aefbf2981d5d2850
Eugene Yavgel <eugene.yavgel@gmail.com>
2021-03-12 13:01:14 +0200

    Listing through orders

--- pages/orders/index.vue
+++ pages/orders/index.vue
@@ -4,25 +4,22 @@
       <div class="columns">
         <div class="column is-12">
           <h1 class="title is-4">Your orders</h1>
-
-          Coming soon ...
-
-<!--          <article class="message" v-if="orders.length">-->
-<!--            <div class="message-body">-->
-<!--              <table class="table is-hoverable is-fullwidth">-->
-<!--                <tbody>-->
-<!--                  <Order-->
-<!--                    v-for="order in orders"-->
-<!--                    :key="order.id"-->
-<!--                    :order="order"-->
-<!--                  />-->
-<!--                </tbody>-->
-<!--              </table>-->
-<!--            </div>-->
-<!--          </article>-->
-<!--          <p v-else>-->
-<!--            You have no orders-->
-<!--          </p>-->
+          <article class="message" v-if="orders.length">
+            <div class="message-body">
+              <table class="table is-hoverable is-fullwidth">
+                <tbody>
+                  <Order
+                    v-for="order in orders"
+                    :key="order.id"
+                    :order="order"
+                  />
+                </tbody>
+              </table>
+            </div>
+          </article>
+          <p v-else>
+            You have no orders
+          </p>
         </div>
       </div>
     </div>
@@ -30,29 +27,29 @@
 </template>
 
 <script>
-  // import Order from '@/components/orders/Order'
-  //
-  // export default {
-  //   data () {
-  //     return {
-  //       orders: []
-  //     }
-  //   },
-  //
-  //   middleware: [
-  //     'redirectIfGuest'
-  //   ],
-  //
-  //   components: {
-  //     Order
-  //   },
-  //
-  //   async asyncData ({ app }) {
-  //     let response = await app.$axios.$get('orders')
-  //
-  //     return {
-  //       orders: response.data
-  //     }
-  //   }
-  // }
+  import Order from '@/components/orders/Order'
+
+  export default {
+    data () {
+      return {
+        orders: []
+      }
+    },
+
+    middleware: [
+      //'redirectIfGuest'
+    ],
+
+    components: {
+      Order
+    },
+
+    async asyncData ({ app }) {
+      let response = await app.$axios.$get('orders')
+
+      return {
+        orders: response.data
+      }
+    }
+  }
 </script>
